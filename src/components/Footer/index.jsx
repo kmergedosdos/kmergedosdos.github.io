128a79352ec98b90a8d4fd155d05901d113c3099
Kmerge Boimilleyr Dosdos <kmerge.dosdos@jg-corporation.com>
2023-05-25 17:35:39 +0800

    update footer layout

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <code className='footer__text'>
-        ©️ 2023. Built with <span>React</span>, <span>JavaScript</span>, and <span>CSS</span> by Kmerge Dosdos. <br />
+        ©️ 2023. Built with <span>React</span>, <span>JavaScript</span>, and <span>CSS</span> by Kmerge Dosdos.
         Design inspired by <a href="https://v4.brittanychiang.com/" target='_blank' rel="noreferrer" className='footer__link'>brittanychiang.com</a>.
       </code>
     </footer>
