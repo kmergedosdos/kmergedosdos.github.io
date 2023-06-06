57efb602f7e9bb865012b231c804b288f5c88656
Kmerge Boimilleyr Dosdos <kmerge.dosdos@jg-corporation.com>
2023-06-06 10:41:30 +0800

    fix width of button

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -7,7 +7,7 @@ function Home() {
       <code>Hello, world! My name is</code>
       <h1 className='home__header'>Kmerge Dosdos.</h1>
       <h4 className='home__subheader'>I design solutions, build software, solve problems, and constantly explore new things.</h4>
-      <a href="#contact">
+      <a href="#contact" style={{width: 'fit-content'}}>
         <button className="btn">Let's connect</button>
       </a>
     </section>
