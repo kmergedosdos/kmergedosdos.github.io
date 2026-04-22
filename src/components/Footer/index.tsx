b39667af7b61b26f5ce44b346a206c44dd52b412
Kmerge Dosdos <kpdosdos@gmail.com>
2026-04-23 03:46:59 +0800

    update footer content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,17 +4,8 @@ function Footer() {
   return (
     <footer className="footer">
       <code className="footer__text">
-        ©️ 2023. Built with <span>React</span>, <span>JavaScript</span>, and{" "}
-        <span>CSS</span> by Kmerge Dosdos. Design inspired by{" "}
-        <a
-          href="https://v4.brittanychiang.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="footer__link"
-        >
-          brittanychiang.com
-        </a>
-        .
+        ©️ 2026 Kmerge Dosdos. Built with <span>React</span>,{" "}
+        <span>TypeScript</span>, and <span>CSS</span>. All rights reserved.
       </code>
     </footer>
   );
